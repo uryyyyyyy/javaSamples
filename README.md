0348d4cf8574b077f90aee10d0d1c0a3eafc8cce
uryyyyyyy <koki305@gmail.com>
2015-12-21 13:00:22 +0900

    add gson, selenium

---
+++ README.md
@@ -0,0 +1,16 @@
+javaSamples
+===========
+
+## setup
+
+gradle wrapper
+
+## tasks
+
+* gradle clean
+* gradle run
+* gradle compileJava
+* gradle test
+
+* gradle --daemon [someTask]
+* gradle --gui
---
+++ javaIO/src/main/java/com/github/uryyyyyyy/javaSamples/javaIO/Main.java
@@ -0,0 +1,9 @@
+package com.github.uryyyyyyy.javaSamples.javaIO;
+
+public class Main {
+
+    public static void main(String args[]){
+        boolean b = new Library().someLibraryMethod();
+        System.out.println(b);
+    }
+}
