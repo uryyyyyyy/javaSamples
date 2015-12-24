f4f17db66dfc50dc89b0be31cbd6aa8d23d07e9a
uryyyyyyy <koki305@gmail.com>
2015-12-24 16:28:07 +0900

    add config

---
+++ config/src/main/java/com/github/uryyyyyyy/javaSamples/config/Main.java
@@ -0,0 +1,24 @@
+package com.github.uryyyyyyy.javaSamples.config;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+
+	public static void main(String[] args) throws IOException {
+		Config config = ConfigFactory.load("application.conf");
+		int s = config.getInt("foo.bar");
+		System.out.println(s);
+
+		Config ss = config.getConfig("foo");
+		System.out.println(ss);
+
+		List<String> sss = config.getStringList("foo.list");
+		System.out.println(sss);
+	}
+
+}
