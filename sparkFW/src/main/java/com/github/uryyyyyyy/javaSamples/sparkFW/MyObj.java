9f6f98d8b2cb0ab568954437c6dacf5e51891092
shiba <koki305@gmail.com>
2015-12-27 16:12:17 +0900

    add spark framework

---
+++ sparkFW/src/main/java/com/github/uryyyyyyy/javaSamples/sparkFW/MyObj.java
@@ -0,0 +1,9 @@
+package com.github.uryyyyyyy.javaSamples.sparkFW;
+
+import java.util.Date;
+
+public class MyObj {
+    public int id;
+    public String name;
+    public Date time;
+}
---
+++ sparkFW/src/test/java/com/github/uryyyyyyy/javaSamples/sparkFW/LibraryTest.java
@@ -0,0 +1,12 @@
+package com.github.uryyyyyyy.javaSamples.sparkFW;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class LibraryTest {
+
+    @Test
+    public void testSomeLibraryMethod() {
+    }
+}
