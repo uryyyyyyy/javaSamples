9312c18284682f5094e03be3ffbb0053de25dcaa
shiba <koki305@gmail.com>
2016-01-03 21:43:34 +0900

    add jooq

---
+++ jooq/src/main/java/com/github/uryyyyyyy/javaSamples/jooq/Main.java
@@ -0,0 +1,8 @@
+package com.github.uryyyyyyy.javaSamples.jooq;
+
+
+public class Main {
+
+    public static void main(String args[]){
+    }
+}
---
+++ jooq/src/test/java/com/github/uryyyyyyy/javaSamples/jooq/GSSampleTest.java
@@ -0,0 +1,11 @@
+package com.github.uryyyyyyy.javaSamples.jooq;
+
+import org.junit.Test;
+
+public class GSSampleTest {
+
+    @Test
+    public void testSelect() {
+    }
+
+}
